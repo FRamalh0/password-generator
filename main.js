f40dc797db9ddb408086f2f5176b45c6ff4703bb
Francisco Ramalho <fc53472@alunos.fc.ul.pt>
2022-09-01 01:13:51 +0100

    v0.2

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 addListeners();
 
 var length = 12;
-var isSpecial = false;
+
+var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charnumbers = "0123456789";
+var charsymbols = "!@#$%^&*()+.";
 
 function addListeners() {
 
@@ -22,26 +25,58 @@ function addListeners() {
 }
 
 function changeCharacter(number) {
+    
     length = number;
     console.log(number);
+
+    for(const element of document.getElementsByClassName('b')) {
+        if(element.innerHTML == number) {
+            element.classList.add('selected');
+        } else {
+            element.classList.remove('selected');
+        }
+    }
+
+    //CUSTOM CLEAN
+    if (number == -1) {
+        document.getElementsByClassName('c')[0].classList.add('selected');
+    } else {
+        document.getElementsByClassName('c')[0].classList.remove('selected');
+    }
+
 }
 
 
 
 function genP() {
 
-    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    if (isSpecial){
-        //TODO
+    let localchar = charset;
+    let locallenght = length;
+
+    //IF NUMBER IS CHECKED
+    if(document.getElementsByClassName("numbers")[0].checked){
+        localchar += charnumbers;
+    }
+    
+    //IF SYMBOLS IS CHECKED
+    if (document.getElementsByClassName("symbols")[0].checked){
+        localchar += charsymbols;
     }
 
-    if(length == -1) {
-        length = document.querySelector('input').value;
+    console.log(localchar);
+
+    if(locallenght == -1) {
+        locallenght = document.querySelector('input').value;
+
+        if(locallenght <= 0) {
+            changeMsgForASecond("Must be a positive value");
+            return;
+        }
     }
 
     let out = "";
-    for (var i = 0; i < length; ++i) {
-        out += charset.charAt(Math.floor(Math.random() * charset.length));
+    for (var i = 0; i < locallenght; ++i) {
+        out += localchar.charAt(Math.floor(Math.random() * localchar.length));
     }
 
     document.getElementsByClassName('p')[0].innerHTML = out;
@@ -49,5 +84,24 @@ function genP() {
 }
 
 function copyToClip() {
+
     navigator.clipboard.writeText(document.getElementsByClassName('p')[0].innerHTML);
+    changeMsgForASecond("Copied to clipboard");
+
+}
+
+function changeMsgForASecond(msg) {
+
+    let currenttext = document.getElementsByClassName("p")[0].innerHTML;
+
+    document.getElementsByClassName("p")[0].innerHTML = msg;
+
+    setTimeout(
+        function() {
+            document.getElementsByClassName("p")[0].innerHTML = currenttext;
+        },
+        1000
+    );
+
+
 }
